927e2a016225c39c0bae527f3bf46c406e9d937d
spin <tgvbnm7980@gmail.com>
2025-01-05 11:55:32 +0800

    內容增加

--- index.js
+++ index.js
@@ -13,21 +13,18 @@ app.use(bodyParser.json());
 // POST 路由
 app.post('/', async (req, res) => {
     try {
-        // 將請求轉發至 Google Apps Script Web App
-        const response = await fetch('https://script.google.com/macros/s/AKfycbww29X-4o2n3_93xry509c9FFVOLtQ4YGZdGLpBlS7QxoUOuXj_sl_VvScFweCHs3OwCQ/exec', {
+        const response = await fetch('https://script.google.com/macros/s/AKfycbzxWTzLvgVWWOgD0jRAHyFk2NQsFsPWBooVzZbvrL4AqfhwgBty-IbRjHSDmANQyUABzA/exec', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(req.body),
+            body: JSON.stringify(req.body), // 將所有表單數據傳遞給 Google Apps Script
         });
-
-        // 獲取 Google Apps Script 的回應
         const result = await response.json();
         res.json(result);
     } catch (error) {
-        console.error('Error:', error);
         res.status(500).json({ message: 'Error connecting to Google Apps Script' });
     }
 });
+
 
 // 啟動伺服器
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
