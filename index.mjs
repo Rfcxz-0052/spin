1973b12e8d5b1af88c0386cd444bf23a27fd4448
spin <tgvbnm7980@gmail.com>
2025-01-27 12:25:45 +0800

    1140127 06

--- index.mjs
+++ index.mjs
@@ -20,7 +20,7 @@ app.post('/', async (req, res) => {
         }
 
         const response = await fetch(
-            'https://script.google.com/macros/s/AKfycbwq0a4vAf2j3lH9NnvKujQxv9amYp6sHRwf33WJ_HhfdF41nyHkJZXfhzWOsdSff_YIxQ/exec',
+            'https://script.google.com/macros/s/AKfycbwWgrhX46r6jPopNdqt4-TzhngiGEdpbMw-s44kDXIo0vSDTe7Vmc-Ta3u3cmprnCdflQ/exec',
             {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
