e8cbacebcac841e1685aba0f48ec42be3d575c1a
spin <tgvbnm7980@gmail.com>
2025-01-28 11:46:07 +0800

    1140128 06

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
 
             try {
-                const response = await fetch(`${"https://script.google.com/macros/s/AKfycbx8reXLju5dfMBl11WyMR-Vsblbj7rBeXT6nPu7NWqoiBN8GQB7Myhdtw6sgm8toJfZ1A/exec"}?query=${encodeURIComponent(query)}`);
+                const response = await fetch(`${"https://script.google.com/macros/s/AKfycbwWYG5mJ0SduB_G53GexNv2c4ShUe1DwCA48Gh2uvsEQC18v6mQbrqIyvYf02z54TtRBQ/exec"}?query=${encodeURIComponent(query)}`);
                 if (!response.ok) {
                     throw new Error('查詢失敗，請稍後再試！');
                 }
